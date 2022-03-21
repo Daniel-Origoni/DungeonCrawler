be413a8ef615cf0bab2386cf746bb394cfef813b
RottingEgo <RottingEgo@gmail.com>
2022-03-20 21:14:18 -0400

    Optimized functions and cleaned code

--- main.py
+++ main.py
@@ -21,14 +21,7 @@ def count(target):
 
 # To move the 'camera' the entire board is shifted.
 def moveMap(self, root):
-    target0 = 350 - self.pos[0]
-    target1 = 275 - self.pos[1]
-
-    position = [0,0]
-
-    position[0] += target0
-    position[1] += target1
-    move = Animation(pos = position, duration = .4)
+    move = Animation(pos = [(350 - self.pos[0]),(275 - self.pos[1])], duration = .4)
 
     move.start(root)
 
@@ -43,37 +36,28 @@ def generateTile(angle, exitID, pos, width):
         
     if hex == 4 or hex == 5 or hex == 7 or hex == 8:
         tile["exitId"] += 100
-        
     if hex == 2 or hex == 5 or hex == 6 or hex == 8:
         tile["exitId"] += 20
-
     if hex == 3 or hex == 6 or hex == 7 or hex == 8:
         tile["exitId"] += 3
-
     if (angle == 0 and exitID == 1) or (angle == 60 and exitID == 2) or (angle == 120 and exitID == 3):
-        tile["position"] = [pos[0] + width / 2, pos[1] + (math.tan(1.0472) * width / 2)]
+        tile["position"] = [round(pos[0] + width / 2, 2), round(pos[1] + (math.tan(1.0472) * width / 2), 2)]
         tile["newAngle"] = 60
-
     if (angle == 60 and exitID == 1) or (angle == 120 and exitID == 2) or (angle == 180 and exitID == 3):
-        tile["position"] = [pos[0] - width / 2, pos[1] + (math.tan(1.0472) * width / 2)]
+        tile["position"] = [round(pos[0] - width / 2, 2), pos[1] + round(math.tan(1.0472) * width / 2, 2)]
         tile["newAngle"] = 120
-        
     if (angle == 120 and exitID == 1) or (angle == 180 and exitID == 2) or (angle == 240 and exitID == 3):
-        tile["position"] = [pos[0] - width, pos[1]]
+        tile["position"] = [round(pos[0] - width, 2), round(pos[1], 2)]
         tile["newAngle"] = 180
-
     if (angle == 180 and exitID == 1) or (angle == 240 and exitID == 2) or (angle == 300 and exitID == 3):
-        tile["position"] = [pos[0] - width / 2, pos[1] - (math.tan(1.0472) * width / 2)]
+        tile["position"] = [round(pos[0] - width / 2, 2), pos[1] - round(math.tan(1.0472) * width / 2, 2)]
         tile["newAngle"] = 240
-
     if (angle == 240 and exitID == 1) or (angle == 300 and exitID == 2) or (angle == 0 and exitID == 3):
-        tile["position"] = [pos[0] + width / 2, pos[1] - (math.tan(1.0472) * width / 2)]
+        tile["position"] = [round(pos[0] + width / 2, 2), pos[1] - round(math.tan(1.0472) * width / 2, 2)]
         tile["newAngle"] = 300
-
     if (angle == 300 and exitID == 1) or (angle == 0 and exitID == 2) or (angle == 60 and exitID == 3):
-        tile["position"] = [pos[0] + width, pos[1]]
+        tile["position"] = [round(pos[0] + width, 2), round(pos[1],2)]
         tile["newAngle"] = 0
-
     return tile
 
 # Function to calculate if the new tile 
@@ -87,7 +71,6 @@ def collides(position, root):
 # Function to place a tile generated with generateTile() onto the board     
 def createTile(self, exitID):
     vacant = True
-
     tile = generateTile(self.angle, exitID, self.pos, self.width)
 
     # Use the collides() function to find vacant spot on the board.
@@ -98,7 +81,7 @@ def createTile(self, exitID):
         
     # If the spot is vacant create a new tile    
     if vacant:
-        self.parent.add_widget(ImageButton(self.parent.tiles, source = 'hex' + str(tile["id"]) + '.png', pos = tile["position"], level = self.level + 1, angle = tile["newAngle"], exitId = tile["exitId"], adjacent = [self.pos]))
+        self.parent.add_widget(ImageButton(self.parent.tiles, source = 'hex' + str(tile["id"]) + '.png', pos = tile["position"], angle = tile["newAngle"], exitId = tile["exitId"], adjacent = [self.pos]))
         self.adjacent.append(tile["position"])
     
 
@@ -107,19 +90,18 @@ class MyGridLayout(GridLayout):
         super().__init__(**kwargs)
 
     def press(MyGridLayout):
-        
+        produce = Animation(opacity = 1, duration = .75)
         child = MyGridLayout.ids.map
 
         if child.children:
             count(child)
             print(child.tiles)
-
         else: 
             child.tiles = {child.width/2-50: {}}
-            child.add_widget(ImageButton(child.tiles, pos = [child.width/2 - 50, child.height/2]))
+            child.add_widget(ImageButton(child.tiles, pos = [round(child.width/2, 2) - 50, round(child.height/2, 2)]))
+            produce.start(child.tiles[child.width/2 - 50][child.height/2])
             
             
-    
     def reset(MyGridLayout):
         MyGridLayout.ids.map.clear_widgets()
         MyGridLayout.ids.map.pos = [0,0]
@@ -128,41 +110,36 @@ class MyGridLayout(GridLayout):
 class ImageButton(ButtonBehavior, Image):
     def __init__(self, tilesList, **kwargs):
         super().__init__(**kwargs)
-        produce = Animation(opacity = 1, duration = .75)
-        produce.start(self)
-        if self.pos[0] not in tilesList:
+        
+        
+        if self.pos[0] not in tilesList:    
             tilesList[self.pos[0]] = {}
         tilesList[self.pos[0]][self.pos[1]] = self
         
-        
-
     def press(self):
         deactivate = Animation(opacity = 0.5, duration = .4)
         activate = Animation (opacity = 1, duration = .4 )
 
         if(self.active):
-            
             moveMap(self, self.parent)
             
             for digit in str(self.exitId):
                 if digit == '0':
                     pass
-                else:
+                else: 
                     createTile(self, int(digit))
 
             for tile in self.adjacent:
                 if self.parent.tiles[tile[0]][tile[1]].current:
                     self.parent.tiles[tile[0]][tile[1]].current = False
+                    
                     for origTileAdj in self.parent.tiles[tile[0]][tile[1]].adjacent:
                         if self.parent.tiles[origTileAdj[0]][origTileAdj[1]] != self:
                             self.parent.tiles[origTileAdj[0]][origTileAdj[1]].active = False
                             deactivate.start(self.parent.tiles[origTileAdj[0]][origTileAdj[1]])
                 self.parent.tiles[tile[0]][tile[1]].active = True
                 activate.start(self.parent.tiles[tile[0]][tile[1]])
-                
-
             self.current = True
-            
         else:
             pass
    
